b9ddd1cbf18e4090120c838db4afa8eef28d4fbc
Saqib Ahmed <saqibahmed-colan>
2023-05-02 12:51:27 +0530

    added view to the front end

--- client/nectar/src/App.js
+++ client/nectar/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import { Link, Route, BrowserRouter as Router } from "react-router-dom";
+import logo from './img/color_icon.png';
+import LoginScreen from './screens/LoginScreen';
+import CreateItemScreen from './screens/CreateItemScreen';
+import React from 'react';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container-fluid">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="header">
+          <img src={logo} className="App-logo"alt="nectar.com" />
+          <h3 className="App-header">Nectar App</h3>
+          </div>
+          <div className="collpase navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="navbar-item">
+                <Link to="/" className="App-link">Login</Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/create" className="App-link">Create Item</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <br />
+        <Route path="/login" component={LoginScreen}/>
+        <Route path="/create" component={CreateItemScreen}/>
+      </div>
+    </Router>
   );
 }
 
